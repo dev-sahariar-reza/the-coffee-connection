b79a479f112279de5e4c5b9e7ff8d638e89b7655
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-10-22 20:17:36 +0600

    error page added

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import errorImage from "../../assets/error/error.jpg";
 
 const Error = () => {
-  return <div></div>;
+  const { error } = useRouteError();
+  return (
+    <section className="my-10">
+      <div className="w-1/4 mx-auto mb-8">
+        <img src={errorImage} alt="error picture" className="rounded-lg" />
+      </div>
+
+      <div className="text-center mb-5">
+        <p className="text-center font-bold text-3xl mb-8">{error?.message}</p>
+        <Link to="/">
+          <button className="btn btn-primary">Go Home</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
