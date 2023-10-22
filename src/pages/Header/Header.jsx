29ffda73b8ec9e55ac992ac94ebd60dc46ef366d
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-10-22 20:19:21 +0600

    header added

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import logo from "../../assets/logo/logo.png";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className="mt-5 mb-10 flex justify-center items-center">
+      <img src={logo} alt="logo" className="w-16 h-16" />
+      <h1 className="font-bold text-3xl">The Coffee Connection</h1>
+    </header>
+  );
 };
 
 export default Header;
